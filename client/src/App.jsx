2fbf7da14ed398905f8317a9569e6a1e11b22bdd
khushi123go <tiwarikhushi907@gmail.com>
2024-04-06 15:58:32 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Login from './Components/Login'
+
+function App() {
+
+  return (
+    <>
+     {/* <h1>app</h1> */}
+     <Login/>
+    </>
+  )
+}
+
+export default App
